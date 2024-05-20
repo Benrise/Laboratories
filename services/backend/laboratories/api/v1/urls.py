421162378dd9165a5c92b4f8d964a7b222515b08
Benrise <benrise_x@mail.ru>
2024-05-20 23:10:29 +0300

    Persons and publications api with docs

--- services/backend/laboratories/api/v1/urls.py
+++ services/backend/laboratories/api/v1/urls.py
@@ -5,4 +5,8 @@ from laboratories.api.v1 import views
 urlpatterns = [
     path('laboratories/', views.LaboratoriesListApi.as_view()),
     path('laboratories/<uuid:pk>', views.LaboratoriesDetailApi.as_view()),
+    path('persons/', views.PersonsListApi.as_view(), name='persons_list'),
+    path('persons/<uuid:pk>/', views.PersonsDetailApi.as_view(), name='person_detail'),
+    path('publications/', views.PublicationsListApi.as_view(), name='publications_list'),
+    path('publications/<uuid:pk>/', views.PublicationsDetailApi.as_view(), name='publication_detail'),
 ]
--- services/backend/laboratories/api/v1/views.py
+++ services/backend/laboratories/api/v1/views.py
@@ -4,7 +4,13 @@ from django.views.generic.detail import BaseDetailView
 from django.db.models import Q
 from django.contrib.postgres.aggregates import ArrayAgg
 
-from laboratories.models import Laboratory, Role
+from laboratories.models import (
+    Laboratory,
+    Publication,
+    PublicationRole,
+    Role,
+    Person
+)
 
 
 class LaboratoryApiMixin:
@@ -58,6 +64,106 @@ class LaboratoriesListApi(LaboratoryApiMixin, BaseListView):
         return context
 
 
-class LaboratoriesDetailApi(LaboratoryApiMixin, BaseDetailView):    
+class LaboratoriesDetailApi(LaboratoryApiMixin, BaseDetailView):
+    def get_context_data(self, **kwargs):
+        return self.get_object()
+
+
+class PersonApiMixin:
+    model = Person
+    http_method_names = ['get']
+
+    def get_queryset(self):
+        qs = Person.objects.prefetch_related(
+            'personlaboratory_set__laboratory',  # Prefetch laboratories related to Person through PersonLaboratory
+            'publications__publicationlaboratory_set__laboratory'  # Prefetch laboratories related to Publication through PublicationLaboratory
+        ).annotate(
+            laboratory_titles=ArrayAgg(
+                'personlaboratory__laboratory__title',
+                distinct=True
+            ),
+            publication_titles=ArrayAgg(
+                'publications__publicationlaboratory__laboratory__title',
+                distinct=True
+            )
+        ).values()
+        return qs
+
+    def render_to_response(self, context, **response_kwargs):
+        return JsonResponse(context)
+
+
+class PersonsListApi(PersonApiMixin, BaseListView):
+    paginate_by = 50
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        qs = self.get_queryset()
+        paginator, page, queryset, is_paginated = self.paginate_queryset(
+            qs,
+            self.paginate_by
+        )
+        context = {
+            'count': paginator.count,
+            'total_pages': page.paginator.num_pages,
+            'prev': page.previous_page_number() if page.number > 1 else None,
+            'next': page.next_page_number() if page.has_next() else None,
+            'results': list(queryset),
+        }
+        return context
+
+
+class PersonsDetailApi(PersonApiMixin, BaseDetailView):
+    def get_context_data(self, **kwargs):
+        return self.get_object()
+
+
+class PublicationApiMixin:
+    model = Publication
+    http_method_names = ['get']
+
+    def get_queryset(self):
+        qs = Publication.objects.prefetch_related('persons').values().annotate(
+            authors=ArrayAgg(
+                'persons__full_name',
+                distinct=True,
+                filter=Q(publicationperson__role=PublicationRole.AUTHOR),
+            ),
+            editors=ArrayAgg(
+                'persons__full_name',
+                distinct=True,
+                filter=Q(publicationperson__role=PublicationRole.EDITOR),
+            ),
+            reviewers=ArrayAgg(
+                'persons__full_name',
+                distinct=True,
+                filter=Q(publicationperson__role=PublicationRole.REVIEWER),
+            ),
+        )
+        return qs
+
+    def render_to_response(self, context, **response_kwargs):
+        return JsonResponse(context)
+
+
+class PublicationsListApi(PublicationApiMixin, BaseListView):
+    paginate_by = 50
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        qs = self.get_queryset()
+        paginator, page, queryset, is_paginated = self.paginate_queryset(
+            qs,
+            self.paginate_by
+        )
+        context = {
+            'count': paginator.count,
+            'total_pages': page.paginator.num_pages,
+            'prev': page.previous_page_number() if page.number > 1 else None,
+            'next': page.next_page_number() if page.has_next() else None,
+            'results': list(queryset),
+        }
+        return context
+
+
+class PublicationsDetailApi(PublicationApiMixin, BaseDetailView):
     def get_context_data(self, **kwargs):
         return self.get_object()
